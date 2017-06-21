7b7035aa05f73c72201b6683f5e4ba50e8f7351a
watchdog1023 <Naickercd@gmail.com>
2017-06-21 16:05:27 +0200

    Add files via upload

--- HQGL_CLASS.h
+++ HQGL_CLASS.h
@@ -11,7 +11,7 @@ private:
               
 public:
     
-void HQGL::HQPlayMP3( char* _MP3_path )
+void HQPlayMP3( char* _MP3_path )
 {
      
      char MP3CmdOp[40] = "open ";
@@ -26,35 +26,35 @@ void HQGL::HQPlayMP3( char* _MP3_path )
      mciSendString( MP3CmdPl,0,0,0 );
 }
 
-void HQGL::HQCloseMP3( char* _MP3_path )
+void HQCloseMP3( char* _MP3_path )
 {
      char MP3CmdCl[40] = "close ";
      strcat( MP3CmdCl,_MP3_path );
      mciSendString( MP3CmdCl,0,0,0 );
 } 
 
-void HQGL::HQPauseMP3( char* _MP3_path )
+void HQPauseMP3( char* _MP3_path )
 {
      char MP3CmdRe[40] = "resume ";
      strcat( MP3CmdRe,_MP3_path );
      mciSendString( MP3CmdRe,0,0,0 );
 } 
  
-void HQGL::HQResumeMP3( char* _MP3_path )
+void HQResumeMP3( char* _MP3_path )
 {
      char MP3CmdPa[40] = "pause ";
      strcat( MP3CmdPa,_MP3_path );
      mciSendString( MP3CmdPa,0,0,0 ); 
 } 
    
-void HQGL::HQStopMP3( char* _MP3_path )
+void HQStopMP3( char* _MP3_path )
 {
      char MP3CmdSt[40] = "stop ";
      strcat( MP3CmdSt,_MP3_path );
      mciSendString( MP3CmdSt,0,0,0 );
 } 
 
-void HQGL::HQGoFullScreenMode( void )
+void HQGoFullScreenMode( void )
 {
      keybd_event(VK_MENU,0x38,0,0);
      keybd_event(VK_RETURN,0x1C,0,0);
@@ -78,86 +78,6 @@ void HQChangeColor( int color )
      SetConsoleTextAttribute( h,color );
 }
 
-//HQConstructor( );
-//~HQDestructor( );
 };
-
-class HQRECTANGLE {
-
-private: 
-         
-public:
-
-void HQRECTANGLE::HQDrawRect( int x,int y,int AB,int AC,int CD,int BD,int color )
-{
-     int d,y1,y2,x1;
-     HANDLE Rect = GetStdHandle( STD_OUTPUT_HANDLE );
-     COORD cursor = { x,y };
-     y1 = y + 1;
-     y2 = y;
-     x1 = x;
-     SetConsoleTextAttribute( Rect,color ); 
-     SetConsoleCursorPosition( Rect,cursor );
-     for( d = 0;d <= AB;d++ )
-          printf( "%lc",196 );
-     cursor.X = x + AB;
-     SetConsoleCursorPosition( Rect,cursor );
-     printf( "%lc",191 );
-     for( d = 0;d <= BD;d++ ){
-          cursor.X = x + AB;cursor.Y = y + 1;
-          SetConsoleCursorPosition( Rect,cursor );
-          printf( "%lc",179 );
-          y++;
-     }
-     for( d = 0;d <= CD-1;d++ ){
-          cursor.X = x;cursor.Y = y + 1;
-          SetConsoleCursorPosition( Rect,cursor );
-          printf( "%lc",196 );
-          x++;
-     }
-     cursor.X = x1 + CD;cursor.Y = y2 + ( BD + 2 );
-     SetConsoleCursorPosition( Rect,cursor );
-     printf( "%lc",217 );
-     for( d = 0;d <= AC;d++ ){
-          cursor.X = x - AB - 1;cursor.Y = y1;
-          SetConsoleCursorPosition( Rect,cursor );
-          printf( "%lc",179 );
-          y1++;
-     }
-     cursor.X = x - AB - 1;cursor.Y = y2;
-     SetConsoleCursorPosition( Rect,cursor );
-     printf( "%lc",218 );
-     cursor.X = x - AB - 1;cursor.Y = y1;
-     SetConsoleCursorPosition( Rect,cursor );
-     printf( "%lc",192 );
-}};
-
-class HQLINE {
-
-private:
-
-public:
-       
-void HQDrawLinexy( int x,int y,int HORIZ_OR_VERT,int length,int color )
-{
-     int x1;
-     HANDLE Line = GetStdHandle( STD_OUTPUT_HANDLE );
-     COORD cursor = { x,y };
-     SetConsoleTextAttribute( Line,color );
-     if( HORIZ_OR_VERT == 1 ){
-         SetConsoleCursorPosition( Line,cursor );
-         for( x1 = 0;x1 <= length;x1++ )
-              printf( "%lc",196 ); // Horizontal Line
-     }
-     else if( HORIZ_OR_VERT == 2 ){
-          for( x1 = 0;x1 <= length;x1++ ){
-               cursor.X = x;
-               cursor.Y = y;
-               SetConsoleCursorPosition( Line,cursor );
-               printf( "%lc",179 );// vertical Line
-               y++;
-          }
-     }
-}};
 
 #endif
