66ff1a7c315e742130f4015f489b046861d409ab
watchdog1023 <Naickercd@gmail.com>
2017-07-09 20:09:34 +0200

    Add files via upload

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 cd "C:\Users\Cameron\Desktop\code projects\C Programming\C++\WIP\AI(Home use)"
-g++ -v -fpermissive -I HQGL_CLASS.h -c Lara.cpp -o Lara.o -Wwrite-strings
-g++ -v -c download.cpp -o download.o -Wwrite-strings
+g++ -v -fpermissive -c Lara.cpp -o Lara.o -Wwrite-strings
+g++ -v -c include\download.cpp -o download.o -Wwrite-strings
 windres icon.rc icon.o
-g++ -v -static -static-libgcc -static-libstdc++ -I HQGL_CLASS.h -o Lara.exe Lara.o download.o icon.o -lwinmm C:\Mingw\lib\libws2_32.a  C:\Mingw\lib\libwininet.a
+g++ -v -static -static-libgcc -static-libstdc++ -o Lara.exe Lara.o download.o icon.o -L. -lchilkat-9.5.0 -lcrypt32 -lws2_32 -ldnsapi -lwinmm -lwininet
 cd "C:\Users\Cameron\Desktop\code projects\C Programming\C++\WIP\AI(Home use)"
