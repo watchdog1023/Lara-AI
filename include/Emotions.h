1ec2d3a681f160ba81bc6822cdc1213bce3031e7
watchdog1023 <Naickercd@gmail.com>
2018-09-27 23:03:30 +0200

    Add files via upload

--- include/Emotions.h
+++ include/Emotions.h
@@ -1,9 +1,15 @@
 #ifndef EMOTIONS
 #define EMOTIONS
+	#include<opencv2/core/core.hpp>
+	#include<opencv2/opencv.hpp>
+	#include<opencv2/objdetect.hpp>
+	#include<opencv2/highgui.hpp>
+	#include<opencv2/imgproc.hpp>
+
 	//Main Reg Function
 	void emot_reg();
 	//The Function that sets the main flags
 	void check();
 	//The function to outline the face
-	void detectAndDraw(Mat& img, CascadeClassifier& cascade,CascadeClassifier& nestedCascade,double scale, bool tryflip);
+	void detectAndDraw(cv::Mat& img, cv::CascadeClassifier& cascade,cv::CascadeClassifier& nestedCascade,double scale, bool tryflip);
 #endif
