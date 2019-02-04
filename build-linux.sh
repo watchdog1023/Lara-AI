c48e1bdbeb883467b686b039caa9687383fdd7cb
watchdog1023 <Naickercd@gmail.com>
2019-02-04 09:14:28 +0200

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -114,9 +114,9 @@ else
     g++ -v -time -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
     g++ -v -time -c include/IRC/IRCSocket.cpp -o IRCSocket.o -Wfatal-errors
     g++ -v -time -c include/IRC/IRCHandler.cpp -o IRCHandler.o -Wfatal-errors
-    g++ -v -time -c include\qr_code\Bitbuffer.cpp -o Bitbuffer.o -Wfatal-errors
-    g++ -v -time -c include\qr_code\QrCode.cpp -o QrCode.o -Wfatal-errors
-    g++ -v -time -c include\qr_code\QrSegment.cpp -o QrSegment.o -Wfatal-errors
+    g++ -v -time -c include/qr_code/Bitbuffer.cpp -o Bitbuffer.o -Wfatal-errors
+    g++ -v -time -c include/qr_code/QrCode.cpp -o QrCode.o -Wfatal-errors
+    g++ -v -time -c include/qr_code/QrSegment.cpp -o QrSegment.o -Wfatal-errors
     g++ -v -time -fpermissive -c include\Emotions.cpp -o Emotions.o -Wfatal-errors
     g++ -time -L"/usr/lib/jvm/java-1.5.0-gcj-6-amd64/lib/" -o lara Lara.o Thread.o IRCClient.o  Bitbuffer.o QrCode.o QrSegment.o Emotions.o IRCSocket.o IRCHandler.o -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_videoio -lopencv_video -lpthread -lsfml-audio -lsfml-network  -lsfml-system -lcurl -lboost_system -lboost_thread -lboost_serialization -lmpi -lpython3.5m -lchilkat-9.5.0 -lmpi_cxx -lncurses -lpocketsphinx -lsphinxbase -lsphinxad
 fi 
