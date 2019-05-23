ef7d1703769534a03975fab5124b4d50301d1f54
watchdog1023 <Naickercd@gmail.com>
2019-05-23 15:05:37 +0200

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -94,15 +94,15 @@ if [  -e /usr/bin/pip3 ]; then
    pip3 install pyscreenshot selenium datetime
 fi
 mpic++ -fpermissive -std=c++14 -I"/usr/lib/jvm/java-8-openjdk-amd64/include/linux/" -I"/usr/lib/jvm/java-8-openjdk-amd64/include/" -c Lara.cpp -o Lara.o -Wfatal-errors 2> LaraC.txt 
-g++ -time -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
-g++ -time -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
-g++ -time -c include/IRC/IRCSocket.cpp -o IRCSocket.o -Wfatal-errors
-g++ -time -c include/IRC/IRCHandler.cpp -o IRCHandler.o -Wfatal-errors
-g++ -time -c include/qr_code/Bitbuffer.cpp -o Bitbuffer.o -Wfatal-errors
-g++ -time -c include/qr_code/QrCode.cpp -o QrCode.o -Wfatal-errors
-g++ -time -c include/qr_code/QrSegment.cpp -o QrSegment.o -Wfatal-errors
-g++ -time -fpermissive -c include/Emotions.cpp -o Emotions.o -Wfatal-errors
-g++ -time -L"/usr/lib/jvm/java-1.5.0-gcj-6-amd64/lib/" -o lara Lara.o Thread.o IRCClient.o  Bitbuffer.o QrCode.o QrSegment.o Emotions.o IRCSocket.o IRCHandler.o -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_videoio -lopencv_video -lpthread -lsfml-audio -lsfml-network  -lsfml-system -lcurl -lboost_system -lboost_thread -lboost_serialization -lmpi -lpython3.5m -lchilkat-9.5.0 -lmpi_cxx -lncurses -lpocketsphinx -lsphinxbase -lsphinxad 2> LaraB.txt
+g++ -time -std=c++14 -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
+g++ -time -std=c++14 -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
+g++ -time -std=c++14 -c include/IRC/IRCSocket.cpp -o IRCSocket.o -Wfatal-errors
+g++ -time -std=c++14 -c include/IRC/IRCHandler.cpp -o IRCHandler.o -Wfatal-errors
+g++ -time -std=c++14 -c include/qr_code/Bitbuffer.cpp -o Bitbuffer.o -Wfatal-errors
+g++ -time -std=c++14 -c include/qr_code/QrCode.cpp -o QrCode.o -Wfatal-errors
+g++ -time -std=c++14 -c include/qr_code/QrSegment.cpp -o QrSegment.o -Wfatal-errors
+g++ -time -std=c++14 -fpermissive -c include/Emotions.cpp -o Emotions.o -Wfatal-errors
+g++ -time -std=c++14  -L"/usr/lib/jvm/java-1.5.0-gcj-6-amd64/lib/" -o lara Lara.o Thread.o IRCClient.o  Bitbuffer.o QrCode.o QrSegment.o Emotions.o IRCSocket.o IRCHandler.o -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_videoio -lopencv_video -lpthread -lsfml-audio -lsfml-network  -lsfml-system -lcurl -lboost_system -lboost_thread -lboost_serialization -lmpi -lpython3.5m -lchilkat-9.5.0 -lmpi_cxx -lncurses -lpocketsphinx -lsphinxbase -lsphinxad 2> LaraB.txt
 if [ ! -e ./lara ];then
     echo "Code Not Sane";
     return 1;
