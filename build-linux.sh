5c49b26b55490a6e4e973674748c623a9d57a6f4
watchdog1023 <Naickercd@gmail.com>
2019-05-25 14:25:42 +0200

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -145,7 +145,120 @@ if [  -e /usr/bin/pip3 ]; then
    sudo -H pip3 install setuptools
    sudo -H pip3 install pyscreenshot selenium datetime
 fi
-sudo sed -i 's/typedef \ long \ long \ int64;/\b\btypedef \ long \ long \ int64;/g' /usr/local/include/sphinxbase/prim_type.h
+sudo rm /usr/local/include/sphinxbase/prim_type.h
+
+echo "#ifndef _LIBUTIL_PRIM_TYPE_H_" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define _LIBUTIL_PRIM_TYPE_H_" >> /usr/local/include/sphinxbase/prim_type.h
+
+echo "/**" >> /usr/local/include/sphinxbase/prim_type.h
+echo "* @file prim_type.h" >> /usr/local/include/sphinxbase/prim_type.h
+echo "* @brief Basic type definitions used in Sphinx. " >> /usr/local/include/sphinxbase/prim_type.h
+echo "*/" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#ifdef __cplusplus" >> /usr/local/include/sphinxbase/prim_type.h
+echo "extern "C" {" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#if 0" >> /usr/local/include/sphinxbase/prim_type.h
+echo "} /* Fool Emacs into not indenting things. */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#include <sphinx_config.h>" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "/* Define some things for VisualDSP++ */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#if defined(__ADSPBLACKFIN__) && !defined(__GNUC__)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "# ifndef HAVE_LONG_LONG" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#  define HAVE_LONG_LONG" >> /usr/local/include/sphinxbase/prim_type.h
+echo "# endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "# ifndef ssize_t" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef signed int ssize_t;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "# endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "# define SIZEOF_LONG_LONG 8" >> /usr/local/include/sphinxbase/prim_type.h
+echo "# define __BIGSTACKVARIABLE__ static" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#else /* Not VisualDSP++ */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "# define __BIGSTACKVARIABLE__" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "/**" >> /usr/local/include/sphinxbase/prim_type.h
+echo "* Union of basic types." >> /usr/local/include/sphinxbase/prim_type.h
+echo "*/" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef union anytype_s {" >> /usr/local/include/sphinxbase/prim_type.h
+echo "void *ptr;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "long i;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "unsigned long ui;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "double fl;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "} anytype_t;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "/*" >> /usr/local/include/sphinxbase/prim_type.h
+echo "* Assume P64 or LP64.  If you need to port this to a DSP, let us know." >> /usr/local/include/sphinxbase/prim_type.h
+echo "*/" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef int		int32;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef short		int16;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef signed char	int8;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef unsigned int	uint32;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef unsigned short	uint16;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef unsigned char	uint8;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef float		float32;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef double		float64;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#if defined(_MSC_VER)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef __int64	         int64;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef unsigned __int64 uint64;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#elif defined(HAVE_LONG_LONG) && (SIZEOF_LONG_LONG == 8)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "//typedef long long	   int64;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef unsigned long long uint64;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#else /* !HAVE_LONG_LONG && SIZEOF_LONG_LONG == 8 */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef double          int64;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "typedef double          uint64;" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif /* !HAVE_LONG_LONG && SIZEOF_LONG_LONG == 8 */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#ifndef TRUE" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define TRUE 1" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#ifndef FALSE" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define FALSE 0" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#ifndef NULL" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define NULL (void *)0" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "/* These really ought to come from <limits.h>, but not everybody has that. */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "/* Useful constants */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_INT32		((int32) 0x7fffffff)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_INT16		((int16) 0x00007fff)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_INT8		((int8)  0x0000007f)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_NEG_INT32		((int32) 0x80000000" >> /usr/local/include/sphinxbase/prim_type.h)
+echo "#define MAX_NEG_INT16		((int16) 0xffff8000)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_NEG_INT8		((int8)  0xffffff80)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_UINT32		((uint32) 0xffffffff)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_UINT16		((uint16) 0x0000ffff)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_UINT8		((uint8)  0x000000ff)" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "/* The following are approximate; IEEE floating point standards might quibble! */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_POS_FLOAT32		3.4e+38f" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_POS_FLOAT32		1.2e-38f	/* But not 0 */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_POS_FLOAT64		1.8e+307" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_POS_FLOAT64		2.2e-308" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_IEEE_NORM_POS_FLOAT32        3.4e+38f" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_IEEE_NORM_POS_FLOAT32        1.2e-38f" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_IEEE_NORM_NEG_FLOAT32       -3.4e+38f" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MAX_IEEE_NORM_POS_FLOAT64        1.8e+307" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_IEEE_NORM_POS_FLOAT64        2.2e-308" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_IEEE_NORM_NEG_FLOAT64       -1.8e+307" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "/* Will the following really work?? */" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_NEG_FLOAT32		((float32) (-MIN_POS_FLOAT32))" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#define MIN_NEG_FLOAT64		((float64) (-MIN_POS_FLOAT64))" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#ifdef __cplusplus" >> /usr/local/include/sphinxbase/prim_type.h
+echo "}" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+echo "" >> /usr/local/include/sphinxbase/prim_type.h
+echo "#endif" >> /usr/local/include/sphinxbase/prim_type.h
+
 mpic++ -fpermissive -std=c++14 -I"/usr/local/include/pocketsphinx/" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"libtensorflow-cpu-linux-x86_64-1.13.1/include" -I"./mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include/jdbc" -I"./mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include/mysql" -I"./mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include/mysqlx" -I"./chilkat-9.5.0-x86_64-linux-gcc/" -I"/usr/lib/jvm/java-8-openjdk-amd64/include/linux/" -I"/usr/lib/jvm/java-8-openjdk-amd64/include/" -c Lara.cpp -o Lara.o -Wfatal-errors 2> LaraC.txt 
 g++ -time -std=c++14 -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
 g++ -time -std=c++14 -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
@@ -155,7 +268,7 @@ g++ -time -std=c++14 -c include/qr_code/BitBuffer.cpp -o Bitbuffer.o -Wfatal-err
 g++ -time -std=c++14 -c include/qr_code/QrCode.cpp -o QrCode.o -Wfatal-errors
 g++ -time -std=c++14 -c include/qr_code/QrSegment.cpp -o QrSegment.o -Wfatal-errors
 g++ -time -std=c++14 -fpermissive -c include/Emotions.cpp -o Emotions.o -Wfatal-errors
-g++ -time -std=c++14 -L"./mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bitlib64" -L"/usr/lib/jvm/java-1.5.0-gcj-6-amd64/lib/" -L"./chilkat-9.5.0-x86_64-linux-gcc/lib" -o lara Lara.o Thread.o IRCClient.o  Bitbuffer.o QrCode.o QrSegment.o Emotions.o IRCSocket.o IRCHandler.o -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_videoio -lopencv_video -lpthread -lsfml-audio -lsfml-network  -lsfml-system -lcurl -lboost_system -lboost_thread -lboost_serialization -lmpi -lpython3.5m -lchilkat-9.5.0 -lmpi_cxx -lncurses -lpocketsphinx -lsphinxbase -lsphinxad 2> LaraB.txt
+g++ -time -std=c++14 -L"./mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/lib64" -L"/usr/lib/jvm/java-1.5.0-gcj-6-amd64/lib/" -L"./chilkat-9.5.0-x86_64-linux-gcc/lib" -o lara Lara.o Thread.o IRCClient.o  Bitbuffer.o QrCode.o QrSegment.o Emotions.o IRCSocket.o IRCHandler.o -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_videoio -lopencv_video -lpthread -lsfml-audio -lsfml-network  -lsfml-system -lcurl -lboost_system -lboost_thread -lboost_serialization -lmpi -lpython3.5m -lchilkat-9.5.0 -lmpi_cxx -lncurses -lpocketsphinx -lsphinxbase -lsphinxad 2> LaraB.txt
 if [ ! -e ./lara ];then
     echo "Code Not Sane";
     echo "========================Build Stage Output========================================"
@@ -163,9 +276,10 @@ if [ ! -e ./lara ];then
     echo "========================Compile Stage Output======================================"
     cat LaraC.txt
     echo "=================================================================================="
-    exit 1;
     if [ $TRAVIS_BRANCH != "master" ]; then
       pause 'Press [Enter] key to continue...'
+    else
+      exit 1;
     fi
 else
     rm -vr lara *.o
