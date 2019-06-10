f56ea9b333787af8c884fa36a37c21d259616253
watchdog1023 <Naickercd@gmail.com>
2019-06-10 18:49:52 +0200

    Update getModels.bat

--- data/Object_data/YOLO/getModels.bat
+++ data/Object_data/YOLO/getModels.bat
@@ -1,1 +1,3 @@
-
+certutil -urlcache -split -f "https://github.com/pjreddie/darknet/raw/master/data/coco.names" coco.names
+certutil -urlcache -split -f "https://pjreddie.com/media/files/yolov3.weights" yolov3.weights
+certutil -urlcache -split -f "https://github.com/pjreddie/darknet/raw/master/cfg/yolov3.cfg" yolov3.cfg
