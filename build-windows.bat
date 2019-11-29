88474683bd39a8dbb46846a9e93300d94b034a9d
watchdog1023 <Naickercd@gmail.com>
2019-11-29 12:32:44 +0200

    Update build-windows.bat

--- build-windows.bat
+++ build-windows.bat
@@ -16,3 +16,18 @@ g++ -v -time -c include/SerialPort.cpp -o SerialPort.o -Wfatal-errors
 windres icon.rc icon.o
 g++ -v -time -static -static-libgcc -static-libstdc++ -L"C:\Development_tools\Interpreters\Python35-32bit\libs" -L"C:\Program Files (x86)\Java\jdk1.8.0_162\lib" -o Lara.exe  icon.o IRCClient.o SerialPort.o IRCSocket.o IRCHandler.o Bitbuffer.o QrCode.o QrSegment.o Thread.o Lara.o download.o -lchilkat-9.5.0 -lgdi32 -lcrypt32 -lws2_32 -lwsock32 -ldnsapi -lwinmm -lwininet -lmingw32 -lopencv_core340.dll -lopencv_highgui340.dll -lopencv_imgproc340.dll -lopencv_imgcodecs340.dll -lopencv_videoio340.dll -lopencv_video340.dll -lboost_system -lboost_serialization -lboost_atomic -lboost_chrono -lboost_thread -lopennn -ltinyxml2 -lpython36 -lpython27 -lzbar.dll -lsfml-audio -lsfml-network -lsfml-main -lsfml-system -lcurl -lbitcoinapi -lcryptopp -ljsoncpp -ltesseract400.dll -lmpi -lmpicxx -lfmpich2g -lpocketsphinx -lsphinxbase
 PAUSE
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
