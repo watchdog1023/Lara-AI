103c60a2c18bf6cbedda2dcf2c284b09f147647d
watchdog1023 <Naickercd@gmail.com>
2019-12-21 12:14:14 +0000

    setting up gitpod enviroment

--- gitpod-2-github.sh
+++ gitpod-2-github.sh
@@ -9,7 +9,7 @@ rm -vr mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit*
 yes | rm -vr libtensorflow-cpu-linux-x86_64-1.13.1*
 rm -vr chilkat-9.5.0-x86_64-linux-gcc*
 rm -vr *.o
-rm -vr data/
+#rm -vr data/
 rm -vr LaraB.txt LaraC.txt
 clear
 echo "Ready to push!"
