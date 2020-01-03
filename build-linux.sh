5263be6cabd74eb2c41a2b58f290e90df3a8db44
watchdog1023 <Naickercd@gmail.com>
2020-01-03 11:47:20 +0200

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -156,27 +156,27 @@ if [ ! -e  assets/ ]; then
     mkdir -v assets/usr/
     mkdir -v assets/usr/local/
 fi
-#if [ ! -e mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/ ]; then
+if [ ! -e mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/ ]; then
    wget -c https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit.tar.gz
    tar xf mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit.tar.gz
    cp -vr mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include/jdbc/* mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include/
    cp -vr mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/lib64/ mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/lib/
    if [ $GITPOD == "YES" ]; then
-      cp -vr mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/* /workspace/Lara-AI/assets/usr/local/
+      cp -vr mysql-*/* /workspace/Lara-AI/assets/usr/local/
    else
       cp -vr mysql-*/* assets/usr/local/
    fi
-#else
-   cp -vr "mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/*" assets/usr/local/
-#fi
+else
+   cp -vr mysql-*/* assets/usr/local/
+fi
 if [ ! -e libtensorflow-cpu-linux-x86_64-1.13.1/ ]; then
    wget -c https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.13.1.tar.gz
    mkdir libtensorflow-cpu-linux-x86_64-1.13.1
    tar xf libtensorflow-cpu-linux-x86_64-1.13.1.tar.gz -C libtensorflow-cpu-linux-x86_64-1.13.1
-   cp -vr libtensorflow-cpu-linux-x86_64-1.13.1/* assets/usr/local/
+   cp -vr libtensorflow-*/* assets/usr/local/
 else
-   ls libtensorflow-cpu-linux-x86_64-1.13.1/
-   cp -vr libtensorflow-cpu-linux-x86_64-1.13.1/* assets/usr/local/
+   ls libtensorflow-*/
+   cp -vr libtensorflow-*/* assets/usr/local/
 fi
 : ' if [ ! -e sphinxbase-5prealpha/ ]; then
    wget --no-check-certificate -c https://tenet.dl.sourceforge.net/project/cmusphinx/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz
