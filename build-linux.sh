b7090d8b52ce2a67d6a0ec30357c2b75c4fced83
watchdog1023 <Naickercd@gmail.com>
2020-01-03 14:47:30 +0200

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -275,6 +275,8 @@ if [ $GITPOD != "YES" ]; then
 elif [ $GITPOD == "YES" ]; then 
     cp include/prim_type.h assets/usr/local/include/sphinxbase/prim_type.h
 fi
+find Python.h
+locate Python.h
 if [ $TRAVIS_BRANCH == "master" ]; then
    mpic++ -fpermissive -std=c++14 -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"/usr/local/include/sphinxbase/" -I"./include/" -I -I"/usr/include/python3.6" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated 2> LaraC.txt 
    g++-8 -time -std=c++14 -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
