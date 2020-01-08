39114e552b56f5672eacc805c9db93a8eb9b39db
watchdog1023 <Naickercd@gmail.com>
2020-01-08 15:25:36 +0000

    updated

--- build-pi.sh
+++ build-pi.sh
@@ -15,7 +15,7 @@ fi
 if [ ! -e /usr/bin/python3 ]; then
    sudo apt-get install python3;
 fi;
-if [ ! -e sphinxbase-5prealpha/ ]; then
+#if [ ! -e sphinxbase-5prealpha/ ]; then
    wget --no-check-certificate -c https://tenet.dl.sourceforge.net/project/cmusphinx/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz
    tar xf sphinxbase-5prealpha.tar.gz
    cd sphinxbase-5prealpha
@@ -23,12 +23,12 @@ if [ ! -e sphinxbase-5prealpha/ ]; then
    make
    sudo make install
    cd ..
-else
-   cd sphinxbase-5prealpha
-   sudo make install
-   cd ..
-fi
-if [ ! -e pocketsphinx-5prealpha/ ]; then
+#else
+#   cd sphinxbase-5prealpha
+#   sudo make install
+#   cd ..
+#fi
+#if [ ! -e pocketsphinx-5prealpha/ ]; then
    wget --no-check-certificate -c https://tenet.dl.sourceforge.net/project/cmusphinx/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz
    tar xf pocketsphinx-5prealpha.tar.gz
    cd pocketsphinx-5prealpha
@@ -36,11 +36,11 @@ if [ ! -e pocketsphinx-5prealpha/ ]; then
    make
    sudo make install
    cd ..
-else
-   cd pocketsphinx-5prealpha
-   sudo make install
-   cd ..
-fi
+#else
+#   cd pocketsphinx-5prealpha
+#   sudo make install
+#   cd ..
+#fi
 if [ $TRAVIS_BRANCH == "master" ]; then
   if [ ! -e mpich-3.3/ ]; then
    wget -c http://www.mpich.org/static/downloads/3.3/mpich-3.3.tar.gz;
