915a13b158c2889e28e254966781228a3c2e25ed
watchdog1023 <Naickercd@gmail.com>
2020-07-26 18:35:49 +0200

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -327,7 +327,7 @@ sudo ldconfig
 sudo updatedb
 if [ $1 == "alive" ]; then
     if [ $TRAVIS_BRANCH == "master" ]; then
-        mpic++ -DALIVE -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated 2> LaraC.txt 
+        mpic++ -DALIVE -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated 2> LaraC.txt 
         g++ -time -std=c++14 -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
         g++ -time -std=c++14 -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
         g++ -time -std=c++14 -c include/IRC/IRCSocket.cpp -o IRCSocket.o -Wfatal-errors
@@ -379,7 +379,7 @@ if [ $1 == "alive" ]; then
             pause 'Press [Enter] key to continue...'
         fi
         if [ $TRAVIS_BRANCH == "master" ]; then
-            mpic++ -DALIVE -v -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated
+            mpic++ -DALIVE -v -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated
             g++ -v -std=c++14 -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
             g++ -v -std=c++14 -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
             g++ -v -std=c++14 -c include/IRC/IRCSocket.cpp -o IRCSocket.o -Wfatal-errors
@@ -420,7 +420,7 @@ if [ $1 == "alive" ]; then
     fi
 else
     if [ $TRAVIS_BRANCH == "master" ]; then
-        mpic++ -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated 2> LaraC.txt 
+        mpic++ -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include"-I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated 2> LaraC.txt 
         g++ -time -std=c++14 -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
         g++ -time -std=c++14 -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
         g++ -time -std=c++14 -c include/IRC/IRCSocket.cpp -o IRCSocket.o -Wfatal-errors
@@ -472,7 +472,7 @@ else
             pause 'Press [Enter] key to continue...'
         fi
         if [ $TRAVIS_BRANCH == "master" ]; then
-            mpic++ -v -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated
+            mpic++ -v -fpermissive -std=c++14 -I"libtitan/C++/" -I"assets/usr/local/include" -I"/usr/local/include/pocketsphinx/" -I"mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include" -I"/usr/local/include/sphinxbase/" -I"./include/" -I"/usr/include/python3.6m/" -c Lara.cpp -o Lara.o -Wfatal-errors -Wdeprecated
             g++ -v -std=c++14 -c include/IRC/Thread.cpp -o Thread.o -Wfatal-errors
             g++ -v -std=c++14 -c include/IRC/IRCClient.cpp -o IRCClient.o -Wfatal-errors
             g++ -v -std=c++14 -c include/IRC/IRCSocket.cpp -o IRCSocket.o -Wfatal-errors
