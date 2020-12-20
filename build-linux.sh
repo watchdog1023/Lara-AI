f329107864172785b6595b3266fc6f2f20fa0041
watchdog1023 <Naickercd@gmail.com>
2020-12-20 15:49:00 +0200

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -193,7 +193,7 @@ fi
        fi
        cd ../..
    fi
-   if [ ! -e mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/ ]; then
+   #if [ ! -e mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/ ]; then
       wget -c https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit.tar.gz
       tar xf mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit.tar.gz
       cp -vr mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include/jdbc/* mysql-connector-c++-8.0.16-linux-glibc2.12-x86-64bit/include/
@@ -203,9 +203,9 @@ fi
       else
          cp -vr mysql-*/* assets/usr/local/
       fi
-   else
-      cp -vr mysql-*/* assets/usr/local/
-   fi
+   #else
+   #   cp -vr mysql-*/* assets/usr/local/
+   #fi
    if [ ! -e libtensorflow-cpu-linux-x86_64-1.13.1/ ]; then
       wget -c https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.13.1.tar.gz
       mkdir libtensorflow-cpu-linux-x86_64-1.13.1
